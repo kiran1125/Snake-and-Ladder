8c18a486cee3806237b54ce857804596d15bb0ed
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-16 22:46:17 +0530

    Added Winning position

--- src/com/bridgeLabz/SnakeAndLadder.java
+++ src/com/bridgeLabz/SnakeAndLadder.java
@@ -4,28 +4,30 @@ public class SnakeAndLadder {
     public static final int START_POSITION=0;
     public static int position=0;
     public static void main(String[] args) {
-            System.out.println("Game started with single player at position " + START_POSITION);
+        System.out.println("Game started with single player at position " + START_POSITION);
+        while (position < 100) {
             Random random = new Random();
             int dice = 1 + random.nextInt(6);
             int options = random.nextInt(3);
-            switch (options){
-                case 0:{
-                    position = position;
-                    System.out.println("No Play and position : " + position);
+            switch (options) {
+                case 0: {
+                    position = position;  //no play
                     break;
                 }
-                case 1:{
-                    position = position + dice;
-                    System.out.println("Ladder and position : " + position);
+                case 1: {
+                    position = position + dice;   //ladder
                     break;
                 }
-                default:{
-                    position = position - dice;
-                    System.out.println("Snake and position : " + position);
+                default: {
+                    position = position - dice;    //snake
+                    if (position < 0)
+                        position=0;
                     break;
                 }
             }
 
+        }
+        System.out.println(position);
     }
 
 
