8f4e84aae32056e671184d714ab17dd1b8ba0e72
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-16 22:02:54 +0530

    Added Rolling Dice

--- src/com/bridgeLabz/SnakeAndLadder.java
+++ src/com/bridgeLabz/SnakeAndLadder.java
@@ -1,9 +1,14 @@
 package com.bridgeLabz;
-
+import java.util.Random;
 public class SnakeAndLadder {
     public static final int START_POSITION=0;
     public static void main(String[] args) {
             System.out.println("Game started with single player at position " + START_POSITION);
+            Random random = new Random();
+            int dice = 1 + random.nextInt(6);
+            System.out.println(dice);
 
-        }
+    }
+
+
 }
