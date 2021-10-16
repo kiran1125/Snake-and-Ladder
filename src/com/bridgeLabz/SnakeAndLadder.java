78b35719445a1bbd4a7899014ea0b69994cb7fb1
kiran1125 <90588261+kiran1125@users.noreply.github.com>
2021-10-16 15:22:52 +0530

    Started Snake And Ladder Program

---
+++ src/com/bridgeLabz/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.bridgeLabz;
+
+public class SnakeAndLadder {
+    public static final int START_POSITION=0;
+    public static void main(String[] args) {
+            System.out.println("Game started with single player at position " + START_POSITION);
+
+        }
+}
